30a66cffca37e1eea3a212080c8aa62bdd29d67c
fabio <dasilvadeoliveirafabio@gmail.com>
2021-11-05 09:51:19 -0300

    teste de deploy 2

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
-
+require('dotenv').config();
+const PORT = process.env.PORT || 3001;
 const route = require('./routes');
 app.use(express.json());
 
 app.use(route)
 
-app.listen(3001)
+app.listen(PORT)
