9c7f94d8ec29b977f887350bf6d22ea7ddc7394a
fabio <dasilvadeoliveirafabio@gmail.com>
2021-11-06 18:51:34 -0300

    teste

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 require('dotenv').config();
 const PORT = process.env.PORT || 3001;
+const cors = require('cors');
+app.use(cors());
 const route = require('./routes');
 app.use(express.json());
 
