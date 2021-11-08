c60486693950b3ed4bee990a59d49881246b49fb
fabio <dasilvadeoliveirafabio@gmail.com>
2021-11-08 15:55:09 -0200

    layout iniciado

--- routes.js
+++ routes.js
@@ -2,10 +2,11 @@ const route = require('express').Router();
 const multer = require('multer');
 const Produto = require('./controllers/controllerProduto');
 const multerConfig = require('./multerConfig')
-route.get('/',(req,res)=>{
-    res.send('<h1>Em desenvolvimento</h1><div><a href="https://api-e-commerce.vercel.app/listar">lista de produtos</a></div>')
-});
+
 route.get('/listar',Produto.listar);
 route.post('/postar',multer(multerConfig).single(),Produto.inserir)
 route.delete("/deletar/:id",Produto.deletar);
+route.get('/',(req,res)=>{
+    res.sendFile(__dirname + '/index.html')
+})
 module.exports = route;
