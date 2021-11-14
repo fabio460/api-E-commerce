529f0406786b495a529dcfa17115639c87ed2e00
fabio <dasilvadeoliveirafabio@gmail.com>
2021-11-14 00:43:27 -0200

    carrinho alterado

--- controllers/controllerCarrinhoDeCompras.js
+++ controllers/controllerCarrinhoDeCompras.js
@@ -9,12 +9,12 @@ exports.inserir = (req,res)=>{
     const sim = modelCarrinho.create({
             nome:req.body.nome,
             imagem1:req.body.imagem1,
-            imagem2:req.body.imagem2,
+           // imagem2:req.body.imagem2,
             valor:req.body.valor,
-            sexo:req.body.sexo,
-            idade:req.body.idade,
-            tipo:req.body.tipo,
-            tecido:req.body.tecido
+           // sexo:req.body.sexo,
+           // idade:req.body.idade,
+           // tipo:req.body.tipo,
+           // tecido:req.body.tecido
     })
     if(sim){
         res.send("campos inseridos com sucesso")
--- models/modelCarrinhoDeCompras.js
+++ models/modelCarrinhoDeCompras.js
@@ -2,12 +2,12 @@ const mongoose = require('mongoose');
 const schema = mongoose.Schema({
     nome:String,
     imagem1:String,
-    imagem2:String,
+    //imagem2:String,
     valor:String,
-    sexo:String,
-    idade:String,
-    tipo:String,
-    tecido:String
+    //sexo:String,
+    //idade:String,
+    //tipo:String,
+   // tecido:String
 })
 const model = mongoose.model('carrinhoDeCompras',schema);
 module.exports = model;
